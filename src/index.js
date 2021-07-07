0cbfef78bdf54634e1222b2502c24ea9d85f317a
Tatsiana Bivoina <tanya.urainbow@mail.ru>
2021-07-07 08:57:35 +0300

    feat: add solution to the task

--- src/index.js
+++ src/index.js
@@ -1,49 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  let chars = str.split('');
-  let charsConfig = bracketsConfig.toString();
-  let stack = [];
-  let open = ['{', '(', '[', '1', '2', '3', '4', '5', '6', '7', '8'];
-  let close = ['}', ')', ']', '1', '2', '3', '4', '5', '6', '7', '8'];
-  let closeIndex;
-  let openIndex;
+  let length = str.length;
 
-  for (var i = 0; i < chars.length; i++) {
-    openIndex = open.indexOf(chars[i]);
-    if (openIndex !== -1) {
-      stack.push(openIndex);
-      continue;
-    }
-    closeIndex = close.indexOf(chars[i]);
-    if (closeIndex !== -1) {
-      openIndex = stack.pop();
-      if (closeIndex !== openIndex) {
-          return false;
-      }
-    }
-    if (stack.length !== 0) {
-      return false;
-    }
-    return true;
+  if (length % 2 == 1) {
+    return false;
   }
-
-  for (let i = 0; i < charsConfig.length; i++) {
-    for(let j = 0; i < charsConfig[i].length; i++){
-      openIndex = open.indexOf(charsConfig[i][j]);
-      if (openIndex !== -1) {
-        stack.push(openIndex);
-        continue;
-      }
-      closeIndex = close.indexOf(charsConfig[i][j]);
-      if (closeIndex !== -1) {
-        openIndex = stack.pop();
-        if (closeIndex !== openIndex) {
-            return false;
-        }
-      }
-      if (stack.length !== 0) {
-        return false;
-      }
+  
+  for(let j = 0; j < length; j++) {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      str = str.replace(bracketsConfig[i].join(''), '');
     }
-    return true;
   }
+  if(str.length > 0) return false
+  else return true
 }
